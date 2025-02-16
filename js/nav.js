ce4514b94916d0bdc466811dd3c995a929c5ac0d
@TheoSempaii <61527540+TheoSempaii@users.noreply.github.com>
2025-02-16 13:27:26 -0300

    updated nav

--- js/nav.js
+++ js/nav.js
@@ -1,35 +1,63 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 
-  const navLogo = document.getElementById("navLogo");
+  const rightSide = document.getElementById("rightSide");
 
-  const timeline = gsap.timeline({
-    paused: true,
 
-  }).to("#navLogo .notHover", {
-    scale: 0,
-    transform: "translateY(5vh)",
-    duration: 0.25,
-    ease: "sine.inOut"
-  }).to("#navLogo .inHover", {
-    scale: 1,
-    transform: "translateY(-5vh)",
-    duration: 0.35,
-    rotate: "360deg",
-    ease: "sine.inOut"
-  })
 
-  navLogo.addEventListener("mouseenter", (e) => {
-    for (let i = 0; i < 10; i++) {
-      createParticle(e.clientX, e.clientY);
+  function generateNavAnimation(genTag, animateOnHover) {
+    const navLogo = document.getElementById(genTag);
+    const timeline = gsap.timeline({
+      paused: true,
+
+    }).to(`#${genTag} .notHover`, {
+      scale: 0,
+      transform: "translateY(5vh)",
+      duration: 0.25,
+      ease: "sine.inOut"
+    }).to(`#${genTag} .inHover`, {
+      scale: 1,
+      transform: "translateY(-5vh)",
+      duration: 0.35,
+      rotate: "360deg",
+      ease: "sine.inOut"
+    })
+
+    if (animateOnHover) {
+      navLogo.addEventListener("mouseenter", (e) => {
+        for (let i = 0; i < 10; i++) {
+          createParticle(e.clientX, e.clientY);
+        }
+        timeline.play()
+
+      });
+
+      navLogo.addEventListener("mouseleave", (e) => {
+
+        timeline.reverse()
+      })
+    } else {
+      let condition = false
+      navLogo.addEventListener("click", (e) => {
+
+        for (let i = 0; i < 10; i++) {
+          createParticle(e.clientX, e.clientY);
+        }
+        if (!condition) {
+          timeline.play()
+
+        } else {
+          timeline.reverse()
+        }
+        rightSide.classList.toggle("openNav")
+        condition = !condition
+
+
+      });
+
+
     }
-    timeline.play()
 
-  });
-
-  navLogo.addEventListener("mouseleave", (e) => {
-
-    timeline.reverse()
-  })
+  }
 
   function createParticle(x, y) {
     const particle = document.createElement("div");
@@ -46,10 +74,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
     particle.style.backgroundColor = Math.random() * 100 < 75 ? "rgb(148, 64, 60)" : "white"
 
 
-    // Animamos la partícula con GSAP
     gsap.to(particle, {
       duration: 2.5,
-      x: (Math.random() - 0.5) * 200, // Movimiento aleatorio
+      x: (Math.random() - 0.5) * 200,
       y: (Math.random() - 0.5) * 200,
       scale: 0,
       opacity: 0,
@@ -57,4 +84,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
       onComplete: () => particle.remove(),
     });
   }
+
+
+  generateNavAnimation("navLogo", true)
+  generateNavAnimation("navToggle")
+
 });
